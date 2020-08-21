7fd7b3e72a43fc98b24045a04672855108e4ca1b
Lexis Dev <lexis.ua@gmail.com>
2020-08-22 01:40:30 +0300

    Implemetation of Arrays - DS.

---
+++ Hackerrank/Arrays/Arrays - DS/Arrays - DS.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the reverseArray function below.
+vector<int> reverseArray(vector<int> a) {
+    vector<int> result;
+
+    for(int i = a.size() - 1; i >= 0; i--) {
+        result.push_back(a[i]);
+    }
+
+    return result;
+}
